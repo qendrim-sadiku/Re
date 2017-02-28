3695d0b11269da2b54c6fe66b1afbd61714a8859
Qendrim Sadiku <denisadiki@gmail.com>
2017-02-28 09:05:53 +0100

    fixed some Issues

---
+++ src/assets/toolkit/scripts/modules/file-input.js
@@ -0,0 +1,54 @@
+$(function() {
+	  if ($('.select-options ul li').hasClass('selected')) {
+	    $('.select .input').text($('.select-options li.selected > span:first-child').text());
+	  } else {
+	    $('.select .input').text($('.select-options li:first-child > span:first-child').text());
+	  }
+	  $('.select').click(function() {
+	    $('.select-options').toggleClass('visible');
+	  });
+	  $('.select-options li').click(function() {
+	    $('.selected').removeClass('selected');
+	    $(this).addClass('selected');
+	    $('.select .input').text($(this).find('span:first-child').text());
+	  });
+});
+
+$(function() {
+	  if ($('.days-select-options ul li').hasClass('selected')) {
+	    $('.days-select .days-input').text($('.days-select-options li.selected > span:first-child').text());
+	  } else {
+	    $('.days-select .days-input').text($('.days-select-options li:first-child > span:first-child').text());
+	  }
+	  $('.days-select').click(function() {
+	    $('.days-select-options').toggleClass('visible');
+	  });
+	  $('.days-select-options li').click(function() {
+	    $('days-select').removeClass('selected');
+	    $(this).addClass('selected');
+	    $('.days-select .days-input').text($(this).find('span:first-child').text());
+	  });
+});
+
+$(function() {
+	  if ($('.hours-select-options ul li').hasClass('selected')) {
+	    $('.hours-select .hours-input').text($('.hours-select-options li.selected > span:first-child').text());
+	  } else {
+	    $('.hours-select .hours-input').text($('.hours-select-options li:first-child > span:first-child').text());
+	  }
+	  $('.hours-select').click(function() {
+	    $('.hours-select-options').toggleClass('visible');
+	  });
+	  $('.hours-select-options li').click(function() {
+	    $('hours-select').removeClass('selected');
+	    $(this).addClass('selected');
+	    $('.hours-select .hours-input').text($(this).find('span:first-child').text());
+	  });
+});
+
+
+
+
+
+    
+
---
+++ src/assets/toolkit/scripts/modules/modal.js
@@ -0,0 +1,9 @@
+$(function() {
+  $("#demo02").animatedModal({
+        modalTarget:'modal-02',
+        animatedIn:'bounceInUp',
+        animatedOut:'bounceOut',        
+        // Callbacks    
+    });
+
+});
